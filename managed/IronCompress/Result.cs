091925b4a1e318bbbaabfbe5b94df081bd7f6720
IG <aloneguid@outlook.com>
2022-10-12 11:18:16 +0100

    "result" adjustments

--- managed/IronCompress/Result.cs
+++ managed/IronCompress/Result.cs
@@ -5,37 +5,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IronCompress
-{
-   /// <summary>
-   /// Operation result with safe disposables.
-   /// </summary>
-   public class Result : IDisposable
-   {
-      private readonly byte[] _data;
-      private readonly int _dataSize;
-      private ArrayPool<byte> _arrayPool;
+namespace IronCompress {
+    /// <summary>
+    /// Operation result with safe disposables.
+    /// </summary>
+    public class Result : IDisposable {
+        private readonly byte[] _data;
+        private readonly int _dataSize;
+        private ArrayPool<byte> _arrayPool;
 
-      internal Result(byte[] data, int dataSize, ArrayPool<byte> arrayPool)
-      {
-         _data = data;
-         _dataSize = dataSize;
-         _arrayPool = arrayPool;
-      }
+        public Result(byte[] data, int dataSize, ArrayPool<byte> arrayPool) {
+            _data = data;
+            _dataSize = dataSize;
+            _arrayPool = arrayPool;
+        }
 
-      public Span<byte> AsSpan() =>
-         _data.AsSpan(0, _dataSize == -1 ? _data.Length : _dataSize);
+        public Result(byte[] data) : this(data, data.Length, null) {
 
-      public static implicit operator Span<byte>(Result r) => r.AsSpan();
+        }
 
-      public static implicit operator ReadOnlySpan<byte>(Result r) => r.AsSpan();
+        public int Length => _dataSize;
 
-      public void Dispose()
-      {
-         if (_arrayPool == null) return;
+        public Span<byte> AsSpan() =>
+           _data.AsSpan(0, _dataSize == -1 ? _data.Length : _dataSize);
 
-         _arrayPool.Return(_data);
-         _arrayPool = null;
-      }
-   }
+        public static implicit operator Span<byte>(Result r) => r.AsSpan();
+
+        public static implicit operator ReadOnlySpan<byte>(Result r) => r.AsSpan();
+
+        public void Dispose() {
+            if(_arrayPool == null)
+                return;
+
+            _arrayPool.Return(_data);
+            _arrayPool = null;
+        }
+    }
 }
