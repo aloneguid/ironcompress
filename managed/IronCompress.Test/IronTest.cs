14752a5f720888f0b80834c00a01ade77141de11
IG <aloneguid@outlook.com>
2022-10-12 11:24:49 +0100

    make tests self-sufficient

--- managed/IronCompress.Test/IronTest.cs
+++ managed/IronCompress.Test/IronTest.cs
@@ -1,34 +1,29 @@
 using System;
 using System.Buffers;
 using IronCompress;
-using NetBox.Generator;
 using Xunit;
 
 namespace IronCompress.Test;
 
-public class IronTest
-{
-   private static readonly Iron _iron = new Iron(ArrayPool<byte>.Shared);
+public class IronTest {
+    private static readonly Iron _iron = new Iron(ArrayPool<byte>.Shared);
+    private readonly Random _rnd = new Random(DateTime.UtcNow.Millisecond);
 
-   [Theory]
-   [InlineData(Codec.Snappy)]
-   [InlineData(Codec.Zstd)]
-   [InlineData(Codec.Gzip)]
-   [InlineData(Codec.Brotli)]
-   [InlineData(Codec.LZO)]
-   [InlineData(Codec.LZ4)]
-   public void EncodeDecodeTest(Codec codec)
-   {
-      int minLength = RandomGenerator.GetRandomInt(10, 1000);
-      int maxLength = RandomGenerator.GetRandomInt(minLength, minLength + 1000);
-      byte[] input = RandomGenerator.GetRandomBytes(minLength, maxLength);
+    [Theory]
+    [InlineData(Codec.Snappy)]
+    [InlineData(Codec.Zstd)]
+    [InlineData(Codec.Gzip)]
+    [InlineData(Codec.Brotli)]
+    [InlineData(Codec.LZO)]
+    [InlineData(Codec.LZ4)]
+    public void EncodeDecodeTest(Codec codec) {
+        byte[] input = new byte[_rnd.Next(100, 10000)];
+        _rnd.NextBytes(input);
 
-      using (Result compressed = _iron.Compress(codec, input.AsSpan()))
-      {
-         using (Result uncompressed = _iron.Decompress(codec, compressed, input.Length))
-         {
-            Assert.Equal(input, uncompressed.AsSpan().ToArray());
-         }
-      }
-   }
+        using(Result compressed = _iron.Compress(codec, input.AsSpan())) {
+            using(Result uncompressed = _iron.Decompress(codec, compressed, input.Length)) {
+                Assert.Equal(input, uncompressed.AsSpan().ToArray());
+            }
+        }
+    }
 }
